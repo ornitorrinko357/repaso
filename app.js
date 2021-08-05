2b009ab05ffef50502a855de276c0dd2388476f3
0rni Torrinko <ornitorrinko357@gmail.com>
2021-08-05 14:41:21 +0200

    fist commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var names = new Array('name1','name2','name3','name4','name5');
+
+var pos = names.lastIndexOf('name2');
+
+console.log(pos);
+
+function goTable(){
+    document.location="table.html";
+}
